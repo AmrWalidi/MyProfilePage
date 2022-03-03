6f69ee42d630e016a6a1787f1b7c7b3a6acb82be
Amr Walidi <amr.nawaf@gmail.com>
2022-03-03 18:57:12 +0300

    hesap makinesi update

--- code.js
+++ code.js
@@ -1,30 +1,30 @@
 function add() {
-    var y = parseInt(document.getElementById("txt1").value);
-    var z = parseInt(document.getElementById("txt2").value);
+    var y = parseInt(document.getElementById("num1").value);
+    var z = parseInt(document.getElementById("num2").value);
     var x = y + z;
-    document.getElementById("result").innerHTML = x;
+    document.getElementById("sonuc").value= x;
   }
   function minus() {
-    var y = parseInt(document.getElementById("txt1").value);
-    var z = parseInt(document.getElementById("txt2").value);
+    var y = parseInt(document.getElementById("num1").value);
+    var z = parseInt(document.getElementById("num2").value);
     var x = y - z;
-    document.getElementById("result").innerHTML = x;
+    document.getElementById("sonuc").value= x;
   }
   function multiply() {
-    var y = parseInt(document.getElementById("txt1").value);
-    var z = parseInt(document.getElementById("txt2").value);
+    var y = parseInt(document.getElementById("num1").value);
+    var z = parseInt(document.getElementById("num2").value);
     var x = y * z;
-    document.getElementById("result").innerHTML = x;
-  }
+    document.getElementById("sonuc").value= x; 
+ }
   function divide() {
-    var y = parseInt(document.getElementById("txt1").value);
-    var z = parseInt(document.getElementById("txt2").value);
+    var y = parseInt(document.getElementById("num1").value);
+    var z = parseInt(document.getElementById("num2").value);
     var x = y / z;
-    document.getElementById("result").innerHTML = x;
-  }
-  function divide() {
-    var y = parseInt(document.getElementById("txt1").value);
-    var z = parseInt(document.getElementById("txt2").value);
+    document.getElementById("sonuc").value= x;  
+}
+  function percent() {
+    var y = parseInt(document.getElementById("num1").value);
+    var z = parseInt(document.getElementById("num2").value);
     var x = y % z;
-    document.getElementById("result").innerHTML = x;
+    document.getElementById("sonuc").value= x;
   }
