e9760e158053d63b042ae0ff2eddf98c40caa520
Amr Walidi <amr.nawaf@gmail.com>
2022-03-17 16:59:27 +0300

    javascript Events

--- code.js
+++ code.js
@@ -28,3 +28,32 @@ function add() {
     var x = y % z;
     document.getElementById("sonuc").value= x;
   }
+  function renkDeğiştirme() {
+    document.getElementById("metin").style = "color: red; font-size: 30px";
+  }
+  const Öğrenci = {
+    adı: "Amr",
+    No: 2210224064,
+    doğumTarih: "12.08.2002",
+  };
+  console.log(Öğrenci);
+
+function big1(){
+  document.getElementById("link1").style="font-size: 35px;position: fixed; left: 60px"
+}
+function big2(){
+  document.getElementById("link2").style="font-size: 35px;position: fixed; right: 680px"
+}
+function big3(){
+  document.getElementById("link3").style="font-size: 35px; position: fixed; right: 20px"
+}
+
+function normal1(){
+  document.getElementById("link1").style="font-size: 25px;position: fixed; left: 60px"
+}
+function normal2(){
+  document.getElementById("link2").style="font-size: 25px;position: fixed; right: 680px"
+}
+function normal3(){
+  document.getElementById("link3").style="font-size: 25px; position: fixed; right: 20px"
+}
