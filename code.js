38a45e1783145be4ebd9ae383f125e2a3c53cbe2
Amr Walidi <amr.nawaf@gmail.com>
2022-03-30 16:40:28 +0300

    project update

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+
 function add() {
     var y = parseInt(document.getElementById("num1").value);
     var z = parseInt(document.getElementById("num2").value);
@@ -40,4 +41,24 @@ function add() {
   };
   console.log(Öğrenci);
 
-
+  var i = 0;
+  var txt ="Web sayfama HOŞ GELDİNİZ! Benim adım Amr Nawaf Walıdı İnönü Üniversitesi öğrenciyim. ";
+  var speed = 60;
+  window.onload = function typeWriter() {
+    if (i < txt.length) {
+      if (i >= 37 && i <= 52) {
+        document.getElementById("demo").innerHTML +=
+          "<span style='color:  rgba(241, 128, 35, 0.993);font-family: cursive;'>" +
+          txt.charAt(i) +
+          "</span>";
+        i++;
+        setTimeout(typeWriter, speed);
+      } else {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+  };
+ 
+ 
