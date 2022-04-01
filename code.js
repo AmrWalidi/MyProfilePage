67517f323dbd185a74ed70cdc7add9004b2928e5
Amr Walidi <amr.nawaf@gmail.com>
2022-04-02 00:10:07 +0300

    new update 2/4/2022

--- code.js
+++ code.js
@@ -1,48 +1,5 @@
-
-function add() {
-    var y = parseInt(document.getElementById("num1").value);
-    var z = parseInt(document.getElementById("num2").value);
-    var x = y + z;
-    document.getElementById("sonuc").value= x;
-  }
-  function minus() {
-    var y = parseInt(document.getElementById("num1").value);
-    var z = parseInt(document.getElementById("num2").value);
-    var x = y - z;
-    document.getElementById("sonuc").value= x;
-  }
-  function multiply() {
-    var y = parseInt(document.getElementById("num1").value);
-    var z = parseInt(document.getElementById("num2").value);
-    var x = y * z;
-    document.getElementById("sonuc").value= x; 
- }
-  function divide() {
-    var y = parseInt(document.getElementById("num1").value);
-    var z = parseInt(document.getElementById("num2").value);
-    var x = y / z;
-    document.getElementById("sonuc").value= x;  
-}
-  function percent() {
-    var y = parseInt(document.getElementById("num1").value);
-    var z = parseInt(document.getElementById("num2").value);
-    var x = y % z;
-    document.getElementById("sonuc").value= x;
-  }
-  
-  function renkDeğiştirme(){
-      document.getElementById("metin").style="color: red; font-size: 30px"; 
-    }
-  
-  const Öğrenci = {
-    adı: "Amr",
-    No: 2210224064,
-    doğumTarih: "12.08.2002",
-  };
-  console.log(Öğrenci);
-
   var i = 0;
-  var txt ="Web sayfama HOŞ GELDİNİZ! Benim adım Amr Nawaf Walıdı İnönü Üniversitesi öğrenciyim. ";
+  var txt ="Web sayfama HOŞ GELDİNİZ! Benim adım Amr Nawaf Walıdı İnönü Üniversitesinde öğrenciyim. ";
   var speed = 60;
   window.onload = function typeWriter() {
     if (i < txt.length) {
