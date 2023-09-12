7a84ca208f966682069dc45de3b82f9ea481481d
Amr Walidi <amr.nawaf@gmail.com>
2023-09-13 00:09:05 +0300

    New Web Site

--- code.js
+++ code.js
@@ -1,21 +1,19 @@
   var i = 0;
   var txt ="Web Sayfama HOŞ GELDİNİZ! Benim Adım AMR WALIDI Yazılım Mühendisliği Okuyorum. ";
-  var speed = 60;
+  txt = txt.split(" ");
+  var speed = 600;
   window.onload = function typeWriter() {
-    if (i < txt.length) {
-      if (i >= 37 && i <= 46) {
-        document.getElementById("demo").innerHTML +=
-          "<span style='color:  rgba(255, 130, 28, 0.993);font-family: Arial; font-weight:bold;'>" +
-          txt.charAt(i) +
-          "</span>";
+    if (i < txt.length) { 
+        document.getElementById("demo").innerHTML += txt[i];
         i++;
         setTimeout(typeWriter, speed);
-      } else {
-        document.getElementById("demo").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-      }
     }
-  };
+  }
+
+
+  function massage() {
+    window.alert("masaj başarlı gönderdi!");
+  }
+ 
  
  
